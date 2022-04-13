8623aceb222e4957a15f922b78d673ff89f1e923
MrProVans <klochkov.30112005@gmail.com>
2022-04-13 23:09:22 +0300

    fc

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-print('ok')
+import logging
+from telegram.ext import Updater, MessageHandler, Filters
+
+# Запускаем логгирование
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = 'BOT_TOKEN'
+
+
+# Определяем функцию-обработчик сообщений.
+# У неё два параметра, сам бот и класс updater, принявший сообщение.
+def echo(update, context):
+    # У объекта класса Updater есть поле message,
+    # являющееся объектом сообщения.
+    # У message есть поле text, содержащее текст полученного сообщения,
+    # а также метод reply_text(str),
+    # отсылающий ответ пользователю, от которого получено сообщение.
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    # Создаём объект updater.
+    # Вместо слова "TOKEN" надо разместить полученный от @BotFather токен
+    updater = Updater(TOKEN)
+
+    # Получаем из него диспетчер сообщений.
+    dp = updater.dispatcher
+
+    # Создаём обработчик сообщений типа Filters.text
+    # из описанной выше функции echo()
+    # После регистрации обработчика в диспетчере
+    # эта функция будет вызываться при получении сообщения
+    # с типом "текст", т. е. текстовых сообщений.
+    text_handler = MessageHandler(Filters.text, echo)
+
+    # Регистрируем обработчик в диспетчере.
+    dp.add_handler(text_handler)
+    # Запускаем цикл приема и обработки сообщений.
+    updater.start_polling()
+
+    # Ждём завершения приложения.
+    # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
+    updater.idle()
+
+
+# Запускаем функцию main() в случае запуска скрипта.
+if __name__ == '__main__':
+    main()
